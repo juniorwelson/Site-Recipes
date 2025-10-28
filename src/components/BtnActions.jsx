4e82d25e521c8e28a4a7ba9f29a25854ddabcb63
welson junior <eujuniorplus@gmail.com>
2025-10-28 20:32:11 -0300

    Adição do componente BtnActions

---
+++ src/components/BtnActions.jsx
@@ -0,0 +1,21 @@
+import "./BtnActions.css"
+
+
+
+const BtnActions = () => {
+  return (
+    <div className="links-action d-flex flex-wrap  ">
+      <a href="#" className="link ">
+        Ache Suas Receitas
+        <img src="/img/lupa.png" className="lupa" alt="lupa" />
+      </a>
+
+      <a href="/recipes" className="link ">
+        Ver Todas as Receitas
+        <img src="/img/salada-icon.png" className="salada-icon" alt="salada-icon" />
+      </a>
+    </div>
+  );
+}
+
+export default BtnActions
