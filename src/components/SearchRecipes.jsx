8450457f6aaa8da76b8d6f77838da4596e6050eb
welson junior <eujuniorplus@gmail.com>
2025-10-29 23:49:48 -0300

    Adicionando o componente da barra da pesquisa

---
+++ src/components/SearchRecipes.jsx
@@ -0,0 +1,22 @@
+import "./SearchRecipes.css";
+const SearchRecipes = ({SearchText,handleChangeText}) => {
+
+  return (  
+    <div className="search-bar d-flex justify-content-center aligns-center">
+      <label htmlFor="bar">
+        Digite o Nome de sua Receita
+        <input
+          type="search"
+          placeholder="Busque sua Receita"
+          name="search-recipes"
+          id="bar"
+          value={SearchText}
+          onChange={handleChangeText}
+        />
+        <img src="/img/lupa.png" alt="lupa" className="logo-lupa" />
+      </label>
+    </div>
+  );
+};
+
+export default SearchRecipes;
