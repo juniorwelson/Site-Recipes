8ca03c60c53fdf23f49c3ecb145ecd8dc81d3055
welson junior <eujuniorplus@gmail.com>
2025-11-04 10:20:21 -0300

    links de opções ao usuario

---
+++ src/components/LinkOptions.jsx
@@ -0,0 +1,23 @@
+
+
+import "./LinkOptions.css"
+const LinkOptions = ({ handlePdfRecipe }) => {
+  return (
+    <div className="Link-options d-flex justify-content-center align-items-center gap-5 flex-wrap">
+      <a href="/recipes" className="fw-bold">
+        Voltar as Receitas
+        <img
+          src="/img/salada-icon.png"
+          className="salada-icon"
+          alt="salada-icon"
+        />
+      </a>
+
+      <button onClick={handlePdfRecipe} id="btn-downlod " className="fw-bold">
+        Baixar Receita <img src="/img/logo-pdf.png" alt="logo pdf" id="logo-pdf" />
+      </button>
+    </div>
+  );
+};
+
+export default LinkOptions
