7aa1173bcc6eee7218b935ed62433ae7674ae6ad
welson junior <eujuniorplus@gmail.com>
2025-11-04 10:43:14 -0300

    footer do site

---
+++ src/components/FooterRecipe.jsx
@@ -0,0 +1,11 @@
+
+const FooterRecipe = () => {
+  return (
+    <footer className="footer-recipe  text-center  py-3 mt-5 bg-success text-white">
+      <p>© 2025 Recipe Site. All rights reserved.</p>
+      <p>Desenvolvido por Welson Junior</p>
+    </footer>
+  );
+};
+
+export default FooterRecipe;
