27a639127bb39e1eaf3196ed2d9d70f9060dc958
welson junior <eujuniorplus@gmail.com>
2025-11-05 10:18:23 -0300

    removi a tag <a> pelo Link do react-router

--- src/components/LinkOptions.jsx
+++ src/components/LinkOptions.jsx
@@ -1,17 +1,17 @@
 
-
+import { Link } from "react-router-dom";
 import "./LinkOptions.css"
 const LinkOptions = ({ handlePdfRecipe }) => {
   return (
     <div className="Link-options d-flex justify-content-center align-items-center gap-5 flex-wrap">
-      <a href="/recipes" className="fw-bold">
+      <Link to="/recipes" className="fw-bold">
         Voltar as Receitas
         <img
           src="/Site-Recipes/img/salada-icon.png"
           className="salada-icon"
           alt="salada-icon"
         />
-      </a>
+      </Link>
 
       <button onClick={handlePdfRecipe} id="btn-downlod " className="fw-bold">
         Baixar Receita{" "}
